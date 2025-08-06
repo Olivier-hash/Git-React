4194733ded2db7b46e58006ae81072afac1a4c40
olivier Rukundo <rr.olivier01@gmail.com>
2025-08-06 22:39:18 +0200

    Added flex-box containers for some components

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -10,7 +10,9 @@ function App() {
   }, []);
 
   return (
+    
     <div className="p-6 font-sans">
+      <div className="container flex flex-cols-3">
       <p className="text-outline 0">input box for user</p>
       <div className="mb-4">    
       </div>
@@ -21,6 +23,7 @@ function App() {
         {messages.map((msg, i) => (
           <p key={i} className="border-b py-5">{msg.text}</p>
         ))}
+      </div>
       </div>
     </div>
   );
