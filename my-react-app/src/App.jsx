173529847b7d1674e381476d298df8eac6cbe92e
olivier Rukundo <rr.olivier01@gmail.com>
2025-08-08 19:38:09 +0200

    applied consistent spacing and formatting

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -12,15 +12,24 @@ function App() {
   return (
     
     <div className="p-6 font-sans">
+
       <div className="container flex flex-cols-3 mb-6">
+
       <p className="text-outline 0">input box for user</p>
-      <div className="mb-4">    
+
+      <div className="mb-4"> 
+
       </div>
       <input type="text" name="" placeholder="choose file"/>
+
       <textarea name="box uper-class" id="">Gift area</textarea>
+
       <textarea name="" id="">Description</textarea>
+
       <div>
+
         {messages.map((msg, i) => (
+          
           <p key={i} className="border-b py-5 flex-2 ">{msg.text}</p>
         ))}
       </div>
