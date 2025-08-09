cf62170d7d8d8abdaaae4dd4620aa649c4dfae1e
olivier Rukundo <rr.olivier01@gmail.com>
2025-08-09 20:57:56 +0200

    implement basic API endpoint with Redis caching layer

--- server.js
+++ server.js
@@ -53,10 +53,6 @@ const redis = require("redis");
 const app = express();
 const PORT = 5000;
 
-// Redis Client
-const client = redis.createClient({
-    url: "redis://127.0.0.1:6379"
-});
 
 client.on("error", (err) => console.error("Redis Client Error", err));
 client.connect();
