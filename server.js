878a002a4c9ca24272191b590c148c872f476e5a
olivier Rukundo <rr.olivier01@gmail.com>
2025-08-09 20:56:49 +0200

    install and configure Redis client in Node.js

--- server.js
+++ server.js
@@ -2,13 +2,6 @@ const express = require("express");
 const fetch = require("node-fetch");
 const redis = require("redis");
 
-// const app = express();
-// const PORT = 5000;
-
-// // Redis Client
-// const client = redis.createClient({
-//     url: "redis://127.0.0.1:6379"
-// });
 
 client.on("error", (err) => console.error("Redis Client Error", err));
 client.connect();
