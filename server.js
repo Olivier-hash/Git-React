b8666b70180bfd6e5380bd174988ba1c301a02d8
olivier Rukundo <rr.olivier01@gmail.com>
2025-08-16 23:36:30 +0200

    Edited code structure

--- server.js
+++ server.js
@@ -5,18 +5,4 @@ const { createClient } = require('redis');
 
 
 
-
-
-
-
-
-app.get('/users', cache, async (req, res) => {
-
-  console.log('Fetching from DB...');
-
-  await redisClient.setEx('users', 60, JSON.stringify(users)); // store for 60 sec
-  
-  res.json(users);
-});
-
 app.listen(3000, () => console.log('Server running on port 3000'));
