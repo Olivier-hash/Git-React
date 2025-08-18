decb828a0628f5e6b6f8cab95e858d9ac1b30cbf
olivier Rukundo <rr.olivier01@gmail.com>
2025-08-18 23:06:34 +0200

    Integrated components into App.js with Router and Routes

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,40 +1,18 @@
-// src/App.js
-import React, { useEffect, useState } from "react";
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
-  
-  useEffect(() => {
-    const messagesRef = ref(database, "messages");
-    
-  }, []);
-
   return (
-    
-    <div className="p-6 font-sans">
-
-      <div className="container flex flex-cols-3 mb-6">
-
-      <p className="text-outline 0">input box for user</p>
-
-      <div className="mb-4"> 
-
-      </div>
-      <input type="text" name="" placeholder="choose file"/>
-
-      <textarea name="box uper-class" id="">Gift area</textarea>
-
-      <textarea name="" id="">Description</textarea>
-
-      <div>
-
-        {messages.map((msg, i) => (
-          
-          <p key={i} className="border-b py-5 flex-2 ">{msg.text}</p>
-        ))}
-      </div>
-      </div>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
