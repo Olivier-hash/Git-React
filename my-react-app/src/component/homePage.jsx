30863f2c6b172ce9dd499c3b1c91cbf2728e8482
olivier Rukundo <rr.olivier01@gmail.com>
2025-08-19 23:48:46 +0200

    Added input box to Homepage

--- my-react-app/src/component/homePage.jsx
+++ my-react-app/src/component/homePage.jsx
@@ -5,6 +5,9 @@ function Home() {
     <div className="p-6">
       <h2 className="text-2xl font-semibold mb-2">Welcome to the Home Page</h2>
       <p className="text-gray-700">This is a random home page component made for testing.</p>
+      <form action="">
+        <input type="text" />
+      </form>
     </div>
   );
 }
