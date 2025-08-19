c11a63519dec6522d8510d97a84e38edbe457819
olivier Rukundo <rr.olivier01@gmail.com>
2025-08-19 23:45:59 +0200

    Modified classname of About page

--- my-react-app/src/component/About.jsx
+++ my-react-app/src/component/About.jsx
@@ -4,7 +4,7 @@ function About() {
   return (
     <div className="p-6">
       <h2 className="text-2xl font-semibold mb-2">About Us</h2>
-      <p className="text-gray-700">
+      <p>
         This is the About component. It just gives some random text about the project.
       </p>
     </div>
