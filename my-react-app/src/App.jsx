a36ee5b9320c535a5211727e36cca7bcf6ff9c87
olivier Rukundo <rr.olivier01@gmail.com>
2025-08-23 23:10:12 +0200

    Removed About page from displaying

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -9,7 +9,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
