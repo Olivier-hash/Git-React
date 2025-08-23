af77402b1048dd46845ac5035c982574cc78f47c
olivier Rukundo <rr.olivier01@gmail.com>
2025-08-23 23:09:06 +0200

    Removed form group

--- my-react-app/src/component/homePage.jsx
+++ my-react-app/src/component/homePage.jsx
@@ -4,9 +4,6 @@ function Home() {
   return (
     <div className="p-6">
       <p className="text-gray-700">This is a random home page component made for testing.</p>
-      <form action="">
-        <input type="text" />
-      </form>
     </div>
   );
 }
