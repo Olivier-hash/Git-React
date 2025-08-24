1801edb92fa5b79eeefcded612c39ea24efbce03
olivier Rukundo <rr.olivier01@gmail.com>
2025-08-24 21:04:23 +0200

    Added Navbar links navigate to / and /about

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-import About from "./components/About";
+
 
 function App() {
   return (
