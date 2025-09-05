93dbc2769684e02e6ccbc40365e1cc890c6cf916
olivier Rukundo <rr.olivier01@gmail.com>
2025-09-05 22:59:37 +0200

    added unit tests for user authentication service

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -6,6 +6,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="Head scroll">
+      <div>
+        <input type="text" className="p-5" />
+      </div>
     </div>
   );
 }
