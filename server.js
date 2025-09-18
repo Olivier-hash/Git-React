af43ead4ad85714756bf465da1b1c3c8e07a2630
olivier Rukundo <rr.olivier01@gmail.com>
2025-09-18 22:15:39 +0200

    Edited Server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-// app.js
-const express = require('express');
-const store = require('main')
-const { createClient,email} = require('redis');
-
-
-
-app.listen(3200, () => console.log('Server running on port 3000'));
