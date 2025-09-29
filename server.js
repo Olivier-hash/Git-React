4d4b806c52662cb1451519c508606c728fab34d6
olivier Rukundo <rr.olivier01@gmail.com>
2025-09-29 22:53:53 +0200

    Added state variables and dotenv file

--- server.js
+++ server.js
@@ -17,16 +17,13 @@ const navigate = useNavigate();
 
             <div className=' top-full right-0 z-20 w-32 p-5 rounded-md
             bg-[#282142] border border-gray-600 text-gray-100 hidden group-hover:block'> 
-              <p onClick={()=> navigate('/profile')} className='cursor-pointer text-sm'>Edit Profile</p>
+              <p className='cursor-pointer text-sm'>Edit Profile</p>
               <hr className='my-2 border-t border-gray-500'/>
               <p className='cursor-pointer text-sm'>Logout</p>
             </div>
-
            </div>
         </div>
-
   
-
       </div>
       {/* users Profile */}
       <div className='flex'>        {userDummyData.map((user,index)=>(
