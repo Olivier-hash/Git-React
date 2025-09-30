ad993e48286fb5ce854cab41eba87baa6d037561
olivier Rukundo <rr.olivier01@gmail.com>
2025-09-30 22:09:08 +0200

    Integrated authentication guard using AuthContext

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ const navigate = useNavigate();
       <div className='pb-33334s'>
         <div className='flex justify-between items-center'>
                    <div className='relative py-2 group'>
-            <img src={assets.menu_icon} alt="Menu" className='max-h-5 cursor-pointer'/>
-
             <div className=' top-full right-0 z-20 w-32 p-5 rounded-md
             bg-[#282142] border border-gray-600 text-gray-100 hidden group-hover:block'> 
               <p className='cursor-pointer text-sm'>Edit Profile</p>
