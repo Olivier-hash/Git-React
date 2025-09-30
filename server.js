d30f3c44b174ab05bd1c2f7921b2aedbc8f9e52b
olivier Rukundo <rr.olivier01@gmail.com>
2025-09-30 22:08:29 +0200

    Added React Router setup (Routes, Route, Navigate)

--- server.js
+++ server.js
@@ -18,8 +18,6 @@ const navigate = useNavigate();
             <div className=' top-full right-0 z-20 w-32 p-5 rounded-md
             bg-[#282142] border border-gray-600 text-gray-100 hidden group-hover:block'> 
               <p className='cursor-pointer text-sm'>Edit Profile</p>
-              <hr className='my-2 border-t border-gray-500'/>
-              <p className='cursor-pointer text-sm'>Logout</p>
             </div>
            </div>
         </div>
